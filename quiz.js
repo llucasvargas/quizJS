e829fea3ac43f47152dbbdb163b0604f6fa7e57b
Lucas vargas <lucas.vargas2000@outlook.com>
2023-05-25 11:43:05 -0300

    Polimento quiz

--- quiz.js
+++ quiz.js
@@ -14,51 +14,61 @@ const perguntas = [
 ]
 
 const opcoes = [
+    
     {id: 1, opcao:
 `a) Uma linguagem de programação.
 b) Um tipo de dado.
 c) Uma sequência de instruções para resolver um problema.
 d) Um erro no código`},
+
     {id: 2, opcao:
 `a) Um resultado de uma expressão matemática.
 b) Um laço de repetição.
 c) Um local de armazenamento para dados.
 d) Uma função pré-definida.`},
+
     {id: 3, opcao:
 `a) Uma estrutura condicional.
 b) Uma função que retorna um valor.
 c) Um tipo de dado.
 d) Uma estrutura de controle para repetir um bloco de código.`},
+
     {id: 4, opcao:
 `a) Uma forma de declarar uma variável.
 b) Um tipo de dado numérico.
 c) Um bloco de código executado várias vezes.
 d) Uma estrutura de controle que permite tomar decisões com base em condições.`},
+
     {id: 5, opcao:
 `a) Um operador matemático.
 b) Um tipo de dado.
 c) Uma estrutura de controle para repetição.
 d) Um bloco de código nomeado que executa uma tarefa específica.`},
+
     {id: 6, opcao:
 `a) Uma função que chama a si mesma.
 b) Um tipo de erro de sintaxe.
 c) Uma forma de depuração de código.
 d) Um operador de atribuição.`},
+
     {id: 7, opcao:
 `a) Um formato para impressão de resultados.
 b) Uma forma de armazenar dados na memória.
 c) Uma função que retorna um valor booleano.
 d) Uma operação matemática.`},
+
     {id: 8, opcao:
 `a) Um comentário no código.
 b) Uma forma de dividir o código em blocos.
 c) Uma estrutura condicional.
 d) Uma operação matemática.`},
+
     {id: 9, opcao:
 `a) O número de linhas de código em um programa.
 b) O tempo necessário para compilar um programa.
 c) A quantidade de recursos que um algoritmo consome.
 d) O número de funções em um programa.`},
+
     {id: 10, opcao:
 `a) A otimização de um algoritmo.
 b) A documentação de um programa.
@@ -86,8 +96,11 @@ let resposta
 let segunda_resposta = 0
 let acertos = 0
 let erros = 0
+let numero_questao = 1
 
 while(i < 10) {
+    console.log('\n=== Quiz Lógica de Programação e Algoritimo ===')
+    console.log('\nPergunta '+ numero_questao)
     console.log(perguntas[i].pergunta)
     console.log(opcoes[i].opcao)
     resposta = entradaDados.question('\nDigite uma opcao: ')
@@ -95,7 +108,10 @@ while(i < 10) {
         pontuacao_total = pontuacao_total + 10
         acertos += 1
         console.log('\nResposta Certa!!\n')
+        numero_questao += 1
     } else {
+        console.log('Resposta errada tente novamente! vale 5 pontos')
+        console.log('\nPergunta '+ numero_questao)
         console.log(perguntas[i].pergunta)
         console.log(opcoes[i].opcao)
         segunda_resposta = entradaDados.question('Digite uma opcao: ')
@@ -103,11 +119,13 @@ while(i < 10) {
         if (segunda_resposta == respostas[i].opcao) {
             pontuacao_total = pontuacao_total + 5
             acertos += 1
+            numero_questao += 1
             console.log('\nResposta Certa!!')
         } else {
             console.log('\nResposta errada!!')
             console.log('\nResposta certa: '+ respostas[i].resposta +'\n')
             erros += 1
+            numero_questao += 1
         }
     }
     console.log('Pontuação: '+ pontuacao_total +'\n')
@@ -125,4 +143,4 @@ if (pontuacao_total == 100) {
 } else {
     console.log('Pessimo!')
 }
-console.log('Você acertou: '+acertos+' e errou: '+erros+' vezes .')
+console.log('Você acertou: '+acertos+' e errou: '+erros+' vezes .\n\n')
