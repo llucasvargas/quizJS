85f3ea43a0120c56e47bc92fee274670df1b06b8
Lucas vargas <lucas.vargas2000@outlook.com>
2023-06-17 11:18:00 -0300

    Updates

---
+++ precache-manifest.001b137e34858b095ba84a23452c1111.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb30a6ee226c91e57053",
+    "url": "/quizReact/static/css/main.9b20bfeb.chunk.css"
+  },
+  {
+    "revision": "fb30a6ee226c91e57053",
+    "url": "/quizReact/static/js/main.fb30a6ee.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/quizReact/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/quizReact/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "371e7ceec4e309afe2b9",
+    "url": "/quizReact/static/js/runtime~main.371e7cee.js"
+  },
+  {
+    "revision": "ee859c8d1bee61ce20a48115ee42caf3",
+    "url": "/quizReact/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/quizJS/precache-manifest.11ce3c05111886cb8c7995fc246f3178.js"
+  "/quizReact/precache-manifest.001b137e34858b095ba84a23452c1111.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/quizJS/index.html", {
+workbox.routing.registerNavigationRoute("/quizReact/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
